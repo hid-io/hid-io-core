d2bdbb71c7bbd6f96a0dae02dc98f402cf57fcda
Jacob Alexander <haata@kiibohd.com>
2021-01-14 21:01:14 -0800

    Splitting commands.rs to mod.rs and test.rs

--- hid-io-protocol/src/commands/mod.rs
+++ hid-io-protocol/src/commands/mod.rs
@@ -33,6 +33,8 @@ use log::{debug, error, warn};
 
 // ----- Modules -----
 
+mod test;
+
 // ----- Macros -----
 
 // ----- Enumerations -----
@@ -136,7 +138,7 @@ pub mod h0002 {
         pub data: Vec<u8, D>,
     }
 
-    pub struct Ack<D: ArrayLength<u8>>  {
+    pub struct Ack<D: ArrayLength<u8>> {
         pub data: Vec<u8, D>,
     }
 
@@ -475,14 +477,14 @@ trait Commands<
         // was serialized
         for pos in (0..data.len()).step_by(<N as Unsigned>::to_usize()) {
             let len = core::cmp::min(<N as Unsigned>::to_usize(), data.len() - pos);
-            match self.tx_bytebuffer().enqueue(
-                match Vec::from_slice(&data[pos..len + pos]) {
+            match self
+                .tx_bytebuffer()
+                .enqueue(match Vec::from_slice(&data[pos..len + pos]) {
                     Ok(vec) => vec,
                     Err(_) => {
                         return Err(CommandError::TxBufferVecTooSmall);
                     }
-                }
-            ) {
+                }) {
                 Ok(_) => {}
                 Err(_) => {
                     return Err(CommandError::TxBufferSendFailed);
@@ -666,8 +668,7 @@ trait Commands<
     fn h0001_info_nak(&self, _data: h0001::Nak) -> Result<(), CommandError> {
         Err(CommandError::IdNotImplemented(HidIoCommandID::SupportedIDs))
     }
-    fn h0001_info_handler(&mut self) -> Result<(), CommandError>
-    {
+    fn h0001_info_handler(&mut self) -> Result<(), CommandError> {
         let buf = self.rx_packetbuffer();
 
         // Handle packet type
@@ -852,8 +853,7 @@ trait Commands<
     fn h0002_test_nak(&self, _data: h0002::Nak) -> Result<(), CommandError> {
         Err(CommandError::IdNotImplemented(HidIoCommandID::SupportedIDs))
     }
-    fn h0002_test_handler(&mut self) -> Result<(), CommandError>
-    {
+    fn h0002_test_handler(&mut self) -> Result<(), CommandError> {
         let buf = self.rx_packetbuffer();
 
         // Handle packet type
@@ -917,458 +917,4 @@ trait Commands<
             _ => Ok(()),
         }
     }
-}
-
-// ----- Tests -----
-
-#[cfg(test)]
-mod test {
-    use super::*;
-    use flexi_logger::Logger;
-    use heapless::consts::{U100, U199, U2, U3, U64, U8};
-
-    enum LogError {
-        CouldNotStartLogger,
-    }
-
-    /// Lite logging setup
-    fn setup_logging_lite() -> Result<(), LogError> {
-        match Logger::with_env_or_str("")
-            .format(flexi_logger::colored_default_format)
-            .format_for_files(flexi_logger::colored_detailed_format)
-            .duplicate_to_stderr(flexi_logger::Duplicate::All)
-            .start()
-        {
-            Err(_) => Err(LogError::CouldNotStartLogger),
-            Ok(_) => Ok(()),
-        }
-    }
-
-    /// Test HID-IO Command Interface
-    struct CommandInterface<
-        TX: ArrayLength<Vec<u8, N>>,
-        RX: ArrayLength<Vec<u8, N>>,
-        N: ArrayLength<u8>,
-        H: ArrayLength<u8>,
-        ID: ArrayLength<HidIoCommandID> + ArrayLength<u8> + Mul<U4> + Add<U5>,
-    > {
-        ids: Vec<HidIoCommandID, ID>,
-        rx_bytebuf: buffer::Buffer<RX, N>,
-        rx_packetbuf: HidIoPacketBuffer<H>,
-        tx_bytebuf: buffer::Buffer<TX, N>,
-    }
-
-    impl<
-            TX: ArrayLength<Vec<u8, N>>,
-            RX: ArrayLength<Vec<u8, N>>,
-            N: ArrayLength<u8>,
-            H: ArrayLength<u8>,
-            ID: ArrayLength<HidIoCommandID> + ArrayLength<u8> + Mul<U4> + Add<U5>,
-        > CommandInterface<TX, RX, N, H, ID>
-    {
-        fn new(ids: &[HidIoCommandID]) -> Result<CommandInterface<TX, RX, N, H, ID>, CommandError> {
-            // Make sure we have a large enough id vec
-            let ids = match Vec::from_slice(ids) {
-                Ok(ids) => ids,
-                Err(_) => {
-                    return Err(CommandError::IdVecTooSmall);
-                }
-            };
-            let tx_bytebuf = buffer::Buffer::new();
-            let rx_bytebuf = buffer::Buffer::new();
-            let rx_packetbuf = HidIoPacketBuffer::new();
-            Ok(CommandInterface {
-                ids,
-                rx_bytebuf,
-                rx_packetbuf,
-                tx_bytebuf,
-            })
-        }
-
-        /// Used to flush the tx_bytebuf into rx_bytebuf
-        /// Effectively creates a loopback
-        fn flush_tx2rx(&mut self) {
-            while !self.tx_bytebuf.is_empty() {
-                if let Some(data) = self.tx_bytebuf.dequeue() {
-                    self.rx_bytebuf.enqueue(data).unwrap();
-                }
-            }
-        }
-    }
-
-    /// CommandInterface for Commands
-    /// NOTE: tx_bytebuf is a loopback buffer
-    ///       rx_bytebuf just reads in tx_buf
-    impl<
-            TX: ArrayLength<Vec<u8, N>>,
-            RX: ArrayLength<Vec<u8, N>>,
-            N: ArrayLength<u8>,
-            H: ArrayLength<u8>,
-            ID: ArrayLength<HidIoCommandID> + ArrayLength<u8> + Mul<U4> + Add<U5>,
-        > Commands<TX, RX, N, H, ID> for CommandInterface<TX, RX, N, H, ID>
-    where
-        <ID as Mul<U4>>::Output: Add<U5>,
-        H: core::fmt::Debug,
-    {
-        fn tx_bytebuffer(&mut self) -> &mut buffer::Buffer<TX, N> {
-            &mut self.tx_bytebuf
-        }
-        fn rx_bytebuffer(&mut self) -> &mut buffer::Buffer<RX, N> {
-            &mut self.rx_bytebuf
-        }
-        fn rx_packetbuffer(&self) -> &HidIoPacketBuffer<H> {
-            &self.rx_packetbuf
-        }
-        fn rx_packetbuffer_mut(&mut self) -> &mut HidIoPacketBuffer<H> {
-            &mut self.rx_packetbuf
-        }
-        fn rx_packetbuffer_clear(&mut self) {
-            self.rx_packetbuf = HidIoPacketBuffer::new();
-        }
-        fn supported_id(&self, id: HidIoCommandID) -> bool {
-            self.ids.iter().any(|&i| i == id)
-        }
-
-        fn h0000_supported_ids_cmd(&self, _data: h0000::Cmd) -> Result<h0000::Ack<ID>, h0000::Nak> {
-            // Build id list to send back
-            Ok(h0000::Ack::<ID> {
-                ids: self.ids.clone(),
-            })
-        }
-        fn h0000_supported_ids_ack(&self, data: h0000::Ack<ID>) -> Result<(), CommandError> {
-            assert!(data.ids == self.ids);
-            Ok(())
-        }
-
-        fn h0001_info_cmd(&self, data: h0001::Cmd) -> Result<h0001::Ack, h0001::Nak> {
-            for entry in &H0001ENTRIES {
-                if entry.property == data.property {
-                    return Ok(h0001::Ack {
-                        property: data.property,
-                        os: entry.os,
-                        number: entry.number,
-                        string: String::from(entry.string),
-                    });
-                }
-            }
-
-            Err(h0001::Nak {
-                property: data.property,
-            })
-        }
-        fn h0001_info_ack(&self, data: h0001::Ack) -> Result<(), CommandError> {
-            // Compare ack with entries
-            for entry in &H0001ENTRIES {
-                if entry.property == data.property
-                    && entry.os == data.os
-                    && entry.number == data.number
-                    && entry.string == data.string
-                {
-                    return Ok(());
-                }
-            }
-
-            Err(CommandError::InvalidProperty8(data.property as u8))
-        }
-
-        fn h0002_test_cmd(&self, data: h0002::Cmd<U256>) -> Result<h0002::Ack<U256>, h0002::Nak> {
-            // Use first payload byte to lookup test entry
-            // Then validate length
-            let entry = &H0002ENTRIES[data.data[0] as usize];
-            if entry.len == data.data.len() {
-                Ok(h0002::Ack {
-                    data: data.data,
-                })
-            } else {
-                Err(h0002::Nak {})
-            }
-        }
-        fn h0002_test_ack(&self, data: h0002::Ack<U256>) -> Result<(), CommandError> {
-            // Use first payload byte to lookup test entry
-            // Then validate length
-            let entry = &H0002ENTRIES[data.data[0] as usize];
-            if entry.len == data.data.len() {
-                Ok(())
-            } else {
-                Err(CommandError::TestFailure)
-            }
-        }
-    }
-
-    // VT TODO
-    // - Print buffer
-    //   * Size should be configurable at build time
-    // - Send message when buffer full, flush, or pattern found (\n)
-
-    // Event Buffers TODO
-    // - KLL Event buffer (maybe have some sort of generic buffer setup?)
-    // - Send buffer on each USB/Output processing cycle
-
-    #[test]
-    fn h0000_supported_ids_test() {
-        setup_logging_lite().ok();
-
-        // Build list of supported ids
-        let ids = [
-            HidIoCommandID::SupportedIDs,
-            HidIoCommandID::GetInfo,
-            HidIoCommandID::TestPacket,
-        ];
-
-        // Setup command interface
-        let mut intf = CommandInterface::<U8, U8, U64, U100, U3>::new(&ids).unwrap();
-
-        // Send command
-        let send = intf.h0000_supported_ids(h0000::Cmd {});
-        assert!(send.is_ok(), "h0000_supported_ids => {:?}", send);
-
-        // Flush tx->rx
-        intf.flush_tx2rx();
-
-        // Process rx buffer
-        let process = intf.process_rx(0);
-        assert!(process.is_ok(), "process_rx1 => {:?}", process);
-
-        // Flush tx->rx
-        intf.flush_tx2rx();
-
-        // Process rx buffer
-        let process = intf.process_rx(0);
-        assert!(process.is_ok(), "process_rx2 => {:?}", process);
-    }
-
-    // Build test entries
-    #[derive(Debug)]
-    struct H0001TestEntry<'a> {
-        property: h0001::Property,
-        os: h0001::OSType,
-        number: u16,
-        string: &'a str,
-    }
-    const H0001ENTRIES: [H0001TestEntry; 13] = [
-        H0001TestEntry {
-            property: h0001::Property::MajorVersion,
-            os: h0001::OSType::Unknown,
-            number: 12,
-            string: "",
-        },
-        H0001TestEntry {
-            property: h0001::Property::MinorVersion,
-            os: h0001::OSType::Unknown,
-            number: 34,
-            string: "",
-        },
-        H0001TestEntry {
-            property: h0001::Property::PatchVersion,
-            os: h0001::OSType::Unknown,
-            number: 79,
-            string: "",
-        },
-        H0001TestEntry {
-            property: h0001::Property::DeviceName,
-            os: h0001::OSType::Unknown,
-            number: 0,
-            string: "My Device",
-        },
-        H0001TestEntry {
-            property: h0001::Property::DeviceSerialNumber,
-            os: h0001::OSType::Unknown,
-            number: 0,
-            string: "1234567890 - 0987654321",
-        },
-        H0001TestEntry {
-            property: h0001::Property::DeviceVersion,
-            os: h0001::OSType::Unknown,
-            number: 0,
-            string: "v9001",
-        },
-        H0001TestEntry {
-            property: h0001::Property::DeviceMCU,
-            os: h0001::OSType::Unknown,
-            number: 0,
-            string: "someMCUname",
-        },
-        H0001TestEntry {
-            property: h0001::Property::FirmwareName,
-            os: h0001::OSType::Unknown,
-            number: 0,
-            string: "SpecialDeviceFirmware",
-        },
-        H0001TestEntry {
-            property: h0001::Property::FirmwareVersion,
-            os: h0001::OSType::Unknown,
-            number: 0,
-            string: "v9999",
-        },
-        H0001TestEntry {
-            property: h0001::Property::DeviceVendor,
-            os: h0001::OSType::Unknown,
-            number: 0,
-            string: "HID-IO",
-        },
-        H0001TestEntry {
-            property: h0001::Property::OsType,
-            os: h0001::OSType::Linux,
-            number: 0,
-            string: "",
-        },
-        H0001TestEntry {
-            property: h0001::Property::OsVersion,
-            os: h0001::OSType::Unknown,
-            number: 0,
-            string: "Special Linux Version",
-        },
-        H0001TestEntry {
-            property: h0001::Property::HostSoftwareName,
-            os: h0001::OSType::Unknown,
-            number: 0,
-            string: "HID-IO Core Unit Test",
-        },
-    ];
-
-    #[test]
-    fn h0001_info_test() {
-        setup_logging_lite().ok();
-
-        // Build list of supported ids
-        let ids = [HidIoCommandID::SupportedIDs, HidIoCommandID::GetInfo];
-
-        // Setup command interface
-        let mut intf = CommandInterface::<U8, U8, U64, U100, U2>::new(&ids).unwrap();
-
-        // Process each of the test entries
-        for entry in &H0001ENTRIES {
-            // Send command
-            let send = intf.h0001_info(h0001::Cmd {
-                property: entry.property,
-            });
-            assert!(send.is_ok(), "h0001_info {:?} => {:?}", entry, send);
-
-            // Flush tx->rx
-            intf.flush_tx2rx();
-
-            // Process rx buffer
-            let process = intf.process_rx(0);
-            assert!(process.is_ok(), "process_rx1 {:?} => {:?}", entry, process);
-
-            // Flush tx->rx
-            intf.flush_tx2rx();
-
-            // Process rx buffer
-            let process = intf.process_rx(0);
-            assert!(process.is_ok(), "process_rx2 {:?} => {:?}", entry, process);
-        }
-    }
-
-    // Build test entries
-    #[derive(Debug)]
-    struct H0002TestEntry {
-        data: [u8; 128],
-        len: usize,
-    }
-    const H0002ENTRIES: [H0002TestEntry; 4] = [
-        // Small message
-        H0002TestEntry {
-            data: [0x00; 128],
-            len: 1,
-        },
-        // Full message
-        H0002TestEntry {
-            data: [0x01; 128],
-            len: 60,
-        },
-        // Multi-packet message
-        H0002TestEntry {
-            data: [0x02; 128],
-            len: 61,
-        },
-        // Full multi-packet message
-        H0002TestEntry {
-            data: [0x03; 128],
-            len: 120,
-        },
-    ];
-
-    #[test]
-    fn h0002_test_test() {
-        setup_logging_lite().ok();
-
-        // Build list of supported ids
-        let ids = [
-            HidIoCommandID::SupportedIDs,
-            HidIoCommandID::GetInfo,
-            HidIoCommandID::TestPacket,
-        ];
-
-        // Setup command interface
-        let mut intf = CommandInterface::<U8, U8, U64, U199, U3>::new(&ids).unwrap();
-
-        for entry in &H0002ENTRIES {
-            // Send command
-            let mut cmd = h0002::Cmd {
-                data: Vec::new(),
-            };
-            for elem in 0..entry.len {
-                cmd.data.push(entry.data[elem]).unwrap();
-            }
-            let send = intf.h0002_test(cmd);
-            assert!(send.is_ok(), "h0002_test {:?} => {:?}", entry, send);
-
-            // Flush tx->rx
-            intf.flush_tx2rx();
-
-            // Process rx buffer
-            let process = intf.process_rx(0);
-            assert!(process.is_ok(), "process_rx1 {:?} => {:?}", entry, process);
-
-            // Flush tx->rx
-            intf.flush_tx2rx();
-
-            // Process rx buffer
-            let process = intf.process_rx(0);
-            assert!(process.is_ok(), "process_rx2 {:?} => {:?}", entry, process);
-        }
-    }
-
-    /*
-    #[test]
-    fn h0016_flashmode_test() {
-        setup_logging_lite().ok();
-
-        // TODO
-        assert!(false, "BLA");
-    }
-
-    #[test]
-    fn h001A_sleepmode_test() {
-        setup_logging_lite().ok();
-
-        // TODO
-        assert!(false, "BLA");
-    }
-
-    #[test]
-    fn h0031_terminalcmd_test() {
-        setup_logging_lite().ok();
-
-        // TODO
-        assert!(false, "BLA");
-    }
-
-    #[test]
-    fn h0034_terminalout_test() {
-        setup_logging_lite().ok();
-
-        // TODO
-        assert!(false, "BLA");
-    }
-
-    #[test]
-    fn hFFFF_invalid_test() {
-        setup_logging_lite().ok();
-
-        // TODO
-        assert!(false, "BLA");
-    }
-    */
 }
